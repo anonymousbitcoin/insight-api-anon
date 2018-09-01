36d617d5fb92103fc7c2285425a7fe82a066c47b
Christopher Arguello <christopheraarguello@gmail.com>
2018-09-01 17:40:13 -0400

    anonify

--- test/blocks.js
+++ test/blocks.js
@@ -3,7 +3,7 @@
 var should = require('should');
 var sinon = require('sinon');
 var BlockController = require('../lib/blocks');
-var bitcore = require('bitcore-lib-btcp');
+var bitcore = require('bitcore-lib-anon-latest');
 var _ = require('lodash');
 
 var blocks = require('./data/blocks.json');
--- test/messages.js
+++ test/messages.js
@@ -3,7 +3,7 @@
 var should = require('should');
 var sinon = require('sinon');
 var MessagesController = require('../lib/messages');
-var bitcore = require('bitcore-lib-btcp');
+var bitcore = require('bitcore-lib-anon-latest');
 var _ = require('lodash');
 
 describe('Messages', function() {
